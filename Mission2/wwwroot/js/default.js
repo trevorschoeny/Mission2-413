f4a40bc5da1044c7134a471007e56c190a4e996e
tschoeny <tschoeny@byu.edu>
2022-01-09 19:04:53 -0700

    Initial check-in of module Mission2

---
+++ Mission2/wwwroot/js/default.js
@@ -0,0 +1,12 @@
+﻿$("#btnSubmit").click(function () {
+
+    var a = Number($("#assignments").val())
+    var g = Number($("#groupProjects").val())
+    var q = Number($("#quizzes").val())
+    var e = Number($("#exams").val())
+    var i = Number($("#intex").val())
+
+    var output = (a * .55) + (g * .05) + (q * .1) + (e * .2) + (i * .1)
+
+    alert("Final Grade: " + output + "%");
+})
